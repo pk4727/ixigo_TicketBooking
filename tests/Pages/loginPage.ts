a4b46c1922e7ea78a41c46ff912f3d339c44c88f
pk4727 <pradhumanpk2018@gmail.com>
2025-10-08 10:15:00 +0530

    login state saved correctly

--- tests/Pages/loginPage.ts
+++ tests/Pages/loginPage.ts
@@ -16,15 +16,11 @@ export class loginPage {
     async fillLoginDetails(mobileNumber: string, filePath: string): Promise<void> {
         await this.page.getByRole('button', { name: 'Log in/Sign up' }).nth(1).click();
         await expect(this.page.locator('#portal-root h5')).toContainText('Log in to ixigo');
-
         await this.page.getByPlaceholder('Enter Mobile Number').fill(`${mobileNumber}`);
         await this.page.getByRole('button', { name: 'Continue' }).click();
-        await this.page.pause(); // Let user manually enter OTP
 
-        // Save session after login
-        const context = this.page.context();
-        await context.storageState({ path: `${filePath}` })
-        
-        expect(this.page.getByText('Pradhuman')).toBeTruthy();
+        await this.page.pause();
+        await this.page.waitForSelector('text=Pradhuman', { timeout: 120000 }); // Wait for user name to appear after login
+        await this.page.context().storageState({ path: filePath });
     }
 }
--- tests/TestCase/assembly.spec.ts
+++ tests/TestCase/assembly.spec.ts
@@ -3,22 +3,14 @@ import { loginPage } from '../Pages/loginPage';
 import { SearchPage } from '../pages/searchPage';
 import { TrainSelection } from '../Pages/trainSelectionPage';
 
-let login: loginPage;
 let searchPage: SearchPage;
 let selectedTrain: TrainSelection;
 
-test.use({'storageState': 'auth.json'});
-test.only('Start to End process', async ({ page }) => {
-    login = new loginPage(page);
+test('Start to End process', async ({ page }) => {
     searchPage = new SearchPage(page);
     selectedTrain = new TrainSelection(page);
-    
 
     await page.pause();
-
-    // await login.navigate();
-    // await login.fillLoginDetails("8409584727", 'auth.json');
-    
     await searchPage.navigate();
     await searchPage.fillJourneyDetails("Pune Jn (PUNE)", "New Delhi (NDLS)", 'tomorrow');
     await searchPage.clickSearch();
